424b24f60c3cb4c710897ddf41e09cd64f7ae0c5
galap <torresjose117@hotmail.com>
2026-09-13 21:45:25 -0500

    Initial commit

---
+++ CalculadoraAPI/Program.cs
@@ -0,0 +1,20 @@
+﻿using CalculadoraAPI.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<ICalculadoraServicio, CalculadoraServicio>();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
